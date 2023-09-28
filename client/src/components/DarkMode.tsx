8488c49e4e4518aaf1fd99a6496c4dfb1fa040bd
Rafael Barbosa <rafael.barbosa51@gmail.com>
2023-09-28 13:08:04 -0500

    Changed the Dark button to a shadcn switch

---
+++ client/src/components/DarkMode.tsx
@@ -0,0 +1,12 @@
+import {Label} from '@/components/ui/label';
+import {Switch} from '@/components/ui/switch';
+
+export default function DarkMode() {
+    const htmlElement = document.querySelector('html')
+    return (
+        <div className='flex items-center gap-2'>
+            <Switch id='dark-mode' onCheckedChange={() => htmlElement?.classList.toggle('dark')}/>
+            <Label htmlFor='dark-mode' className='dark:text-[#f5e4e4] text-xl text-[#ebadad] drop-shadow-md '>Dark Mode</Label>
+        </div>
+    )
+}
--- client/src/pages/Layout.tsx
+++ client/src/pages/Layout.tsx
@@ -3,19 +3,20 @@ import { Card} from "@/components/ui/card";
 import {BsLightbulb} from 'react-icons/bs';
 import { Outlet, NavLink} from "react-router-dom";
 import {NavigationMenu, NavigationMenuItem, NavigationMenuLink, navigationMenuTriggerStyle} from "@/components/ui/navigation-menu"
-
+import DarkMode from "@/components/DarkMode";
 
 export default function Layout() {
-    const htmlElement = document.querySelector('html')
+    //const htmlElement = document.querySelector('html')
     return (
         <>
         <div className="h-screen bg-lightPattern dark:bg-darkPattern p-4 opacity-80" >
-            <Card className="backdrop-blur-lg bg-white/60 dark:bg-black/40 drop-shadow-xl mx-8">
+            {/* backdrop-blur-lg bg-white/60 dark:bg-black/40 drop-shadow-xl mx-8 */}
+            <Card className="flex flex-col backdrop-blur-sm bg-white/20 dark:bg-black/40 drop-shadow-xl mx-8">
               <div className="flex justify-between align-middle p-2">
-                {/* <div className=""> */}
-                  <h1 className="font-Baskervville text-5xl text-pinkPrimary drop-shadow-sm dark:text-pinkPrimary">Girl Boss App</h1>
-                  <Button className="rounded-md backdrop-blur-sm text-slate-900 bg-white/60 hover:bg-slate-300/60 dark:bg-white/40 drop-shadow-xl self-center" onClick={() => htmlElement?.classList.toggle('dark')}><BsLightbulb /></Button>
-                {/* </div> */}
+                {/* font-Baskervville text-5xl text-pinkPrimary drop-shadow-sm dark:text-pinkPrimary */}
+                <h1 className="font-Baskervville text-center text-5xl text-[#ebadad] drop-shadow-md dark:text-[#f5e4e4]">Girl Boss App</h1>
+                {/* <Button className="rounded-md backdrop-blur-sm text-slate-900 bg-white/60 hover:bg-slate-300/60 dark:bg-white/40 drop-shadow-xl self-center" onClick={() => htmlElement?.classList.toggle('dark')}><BsLightbulb /></Button>  */}
+                <DarkMode />
               </div>
             </Card>
             
