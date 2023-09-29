15d97d33a2faf919b3a28e29d853db24ac6a73a4
Rafael Barbosa <rafael.barbosa51@gmail.com>
2023-09-29 16:10:42 -0500

    Scheduler Partially complete

--- client/src/pages/Kanban.tsx
+++ client/src/pages/Kanban.tsx
@@ -1,16 +1,11 @@
 import './Kanban.css';
 import { KanbanComponent, ColumnsDirective, ColumnDirective } from '@syncfusion/ej2-react-kanban';
 import {data} from '@/data/kanbanData';
-import type {dataType} from '@/data/kanbanData'
 import { Card } from '@/components/ui/card';
+import { Separator } from '@/components/ui/separator';
 
 
 function Kanban() {
-    
-    const addContent = (newData: dataType) => {
-        const size = data.length;
-        data.push({Id: size, Status: newData.Status, Summary: newData.Summary})
-    }
 
   return (
     
@@ -18,8 +13,13 @@ function Kanban() {
       <h1 className="text-3xl font-Baskervville text-center drop-shadow-md">Task Board</h1>
       <p className='text-center indent-4 mb-2 mx-4'>
         This Task Board (Kanban) board will help you organize your tasks based on completion status. This can be extremely useful in organizing your ideas and relieve stress.
-        <strong>Drag and drop</strong> tasks from column to columns. <strong>Double click</strong> a task to edit it. On the dialog box, write your task under summary and then click <strong>save</strong>.
       </p>
+      <Separator className='w-[90%] mx-auto mt-2 mb-4 bg-slate-400' />
+      <ul className='w-[50%] mx-auto list-disc my-2 text-center'>
+        <li><strong>Drag and drop</strong> tasks from column to columns.</li>
+        <li><strong>Double click</strong> a task to edit it.</li>
+        <li>On the dialog box, write your task under summary and then click <strong>save</strong>.</li>
+      </ul>
       <KanbanComponent id='kanban' keyField='Status' dataSource={data} cardSettings={{contentField: "Summary", headerField: "Id"}}>
         <ColumnsDirective>
           <ColumnDirective headerText='To do' keyField='ToDo' />
--- client/src/pages/Scheduler.tsx
+++ client/src/pages/Scheduler.tsx
@@ -1,6 +1,43 @@
+import './Scheduler.css';
+import { ScheduleComponent, Day, Week, WorkWeek, Month, ViewsDirective, ViewDirective, Agenda, Inject } from '@syncfusion/ej2-react-schedule';
+import { Card } from '@/components/ui/card';
+import { Separator } from '@/components/ui/separator';
 
 export default function Scheduler() {
+    const data = [
+        {
+          Id: 1,
+          Subject: 'My first task',
+          StartTime: new Date(2023,10,15,10,0),
+          EndTime: new Date(2023,10,15,12, 30)
+        },
+      ];
+
     return (
-        <div>Scheduler</div>
+        <Card className='p-4 bg-red-200/40 rounded-lg shadow-md'>
+            <h1 className="text-3xl font-Baskervville text-center drop-shadow-md">Scheduler</h1>
+            <p className='text-center indent-4 mb-2 mx-4'>
+              Use this scheduler to organize your tasks. 
+              
+            </p>
+            <Separator className='w-[90%] mx-auto mt-2 mb-4 bg-slate-400' />
+            <ul className='w-[50%] mx-auto list-disc my-2 text-center'>
+                <li><strong>Click</strong> on a timeframe (Day, Week, Month)</li>
+                <li><strong>Click</strong> on a time block</li>
+                <li>On the new dialog box, choose <strong>More Details</strong></li>
+                <li>On the menu, fill out the corresponding fields</li>
+            </ul>
+
+            {/* eventSettings={{dataSource: data,}} */}
+            <ScheduleComponent  eventSettings={{dataSource: data,}} >
+                <ViewsDirective>
+                  <ViewDirective option='Day' startHour='09:00' endHour='17:00'/>
+                  <ViewDirective option='WorkWeek' startHour='09:00' endHour='17:00'/>
+                  <ViewDirective option='Week' startHour='09:00' endHour='17:00'/>
+                  <ViewDirective option='Month' showWeekend={false}/>
+                </ViewsDirective>
+                <Inject services={[Day, Week, WorkWeek, Month, Agenda]} />
+            </ScheduleComponent>
+        </Card>
     )
 }
