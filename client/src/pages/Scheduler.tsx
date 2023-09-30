a3d0bc796b911125f5217f22ee54978573450f9b
Rafael Barbosa <rafael.barbosa51@gmail.com>
2023-09-30 00:11:42 -0500

    Scheduler now saves data

--- client/src/pages/Scheduler.tsx
+++ client/src/pages/Scheduler.tsx
@@ -3,22 +3,47 @@ import { ScheduleComponent, Day, Week, WorkWeek, Month, ViewsDirective, ViewDire
 import { Card } from '@/components/ui/card';
 import { Separator } from '@/components/ui/separator';
 import { useRef } from 'react';
+import {useStorageState} from 'react-use-storage-state';
 
 export default function Scheduler() {
-    const data = [
-        {
-          Id: 1,
-          Subject: 'My first task',
-          StartTime: new Date(2023,10,15,10,0),
-          EndTime: new Date(2023,10,15,12, 30)
-        },
-      ];
-      const scheduleObj = useRef<ScheduleComponent>(null)
-      const doSomething = () => {
-        data[0].Subject = "New Task Name  HOOOWOWO!!!"
-        scheduleObj.current?.refresh()
-        console.log(data);
-      }
+    const initData = [
+      {
+        Id: 1,
+        Subject: 'My first task',
+        StartTime: new Date(2023,10,15,10,0),
+        EndTime: new Date(2023,10,15,12, 30)
+      },
+    ];
+    const [data, setData] = useStorageState('Data', initData)
+    const scheduleObj = useRef<ScheduleComponent>(null)
+    const doSomething = () => {
+      //initData[0].Subject = "New Task Name  HOOOWOWO!!!"
+      scheduleObj.current?.refresh()
+      // scheduleObj.current?.exportToExcel();
+      console.log(initData);
+      console.log(data);
+      setData([...data])
+    }
+    
+    
+
+//     const onActionBegin = (args: any) => {
+//       if (args.requestType === 'toolbarItemRendering') {
+//           let exportItem = {
+//               align: 'Right', showTextOn: 'Both', prefixIcon: 'e-icon-schedule-excel-export',
+//               text: 'Excel Export', cssClass: 'e-excel-export', click: onExportClick
+//           };
+//           args.items.push(exportItem);
+//       }
+//   }
+//   const onExportClick = () => {
+//     let exportValues = {
+//       fields: ['Id', 'Subject', 'StartTime', 'EndTime', 'Location']
+//   };
+//     scheduleObj.current?.exportToExcel(exportValues);
+// }
+
+
     return (
         <Card className='p-4 bg-red-200/40 rounded-lg shadow-md'>
             <h1 className="text-3xl font-Baskervville text-center drop-shadow-md">Scheduler</h1>
@@ -35,7 +60,7 @@ export default function Scheduler() {
             </ul>
 
             {/* eventSettings={{dataSource: data,}} */}
-            <ScheduleComponent ref={scheduleObj} eventSettings={{dataSource: data,}} >
+            <ScheduleComponent ref={scheduleObj} eventSettings={{dataSource: data}}>
                 <ViewsDirective>
                   <ViewDirective option='Day' startHour='09:00' endHour='17:00'/>
                   <ViewDirective option='WorkWeek' startHour='09:00' endHour='17:00'/>
