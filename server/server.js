1afbc367e162f7af952593aca4a4fb0a876104ca
Rafael Barbosa <rafael.barbosa51@gmail.com>
2023-09-30 21:09:54 -0500

    Prisma configured on server to MongoDB Atlas

--- server/server.js
+++ server/server.js
@@ -1,9 +1,17 @@
 const express = require('express')
 const app = express();
+require('dotenv').config()
+const {PrismaClient} = require('@prisma/client')
+const prisma = new PrismaClient()
 const PORT = process.env.PORT || 5000;
 const path = require('path');
 
 app.use(express.static(path.join(__dirname, '../client', 'dist')))
+app.get('/mongo', async (req, res) => {
+    const comments = await prisma.comment.findMany()
+    res.json(comments);
+})
+//app.post()
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
