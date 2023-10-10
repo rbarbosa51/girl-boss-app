d33c6b7784baefc1c31f00a5fc74f0a4b02a6105
Rafael Barbosa <rafael.barbosa51@gmail.com>
2023-10-10 15:46:12 -0500

    Removed React-Query

--- src/main.tsx
+++ src/main.tsx
@@ -12,8 +12,7 @@ import {
 } from "@/pages";
 import "@/index.css";
 import { registerLicense } from "@syncfusion/ej2-base";
-import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
-const client = new QueryClient()
+
 
 const router = createBrowserRouter([
   {
@@ -47,7 +46,7 @@ const router = createBrowserRouter([
 registerLicense(import.meta.env.VITE_SYNCFUSION_KEY);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <QueryClientProvider client={client}>
+  <React.StrictMode>
     <RouterProvider router={router} />
-  </QueryClientProvider>,
+  </React.StrictMode>,
 );
